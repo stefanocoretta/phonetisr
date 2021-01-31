31e98fa92d09132cca60237cb898d2895317d0d5
stefanocoretta <stefano.coretta@gmail.com>
2021-01-31 14:34:18 +0100

    :sparkles: - document data, add example

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Klingon Swadesh list
+#'
+#' The Swadesh list in Klingon.
+#'
+#' @format A data frame with 195 rows and 4 variables:
+#' \describe{
+#'   \item{id}{Swadesh list item number.}
+#'   \item{gloss}{English gloss.}
+#'   \item{translit}{Klingon transliteration.}
+#'   \item{ipa}{IPA transcription.}
+#' }
+"kl_swadesh"
