680ae09ed9026baf4cfc2af5818703d258170154
stefanocoretta <stefano.coretta@gmail.com>
2021-06-07 18:40:19 +0200

    ♻️ - add argument to tokenise with default

--- R/phonetise.R
+++ R/phonetise.R
@@ -11,6 +11,7 @@
 #' @param regex A string with a regular expression to match several
 #'   multi-character phones.
 #' @param sanitise Whether to remove all non-IPA characters (`TRUE` by default).
+#' @param default_multi If set to `TRUE`, parses all valid diacritics as part of the previous character (`FALSE` by default).
 #' @param sanitize Alias of `sanitize`.
 #'
 #' @return A list.
@@ -65,6 +66,16 @@ phonetise <- function(strings, multi = NULL, regex = NULL, sanitise = TRUE,
 
     multi_len <- multi_len + length(multi_rx)
     multi <- c(multi, multi_rx)
+  }
+
+  # Use default diacritic list for multichar symbols
+  if (default_multi) {
+    multi_dia <- stringr::str_extract_all(strings, diacritics_regex) %>%
+      unlist() %>%
+      unique()
+
+    multi_len <- multi_len + length(multi_dia)
+    multi <- c(multi, multi_dia)
   }
 
   ####
@@ -130,3 +141,17 @@ ipa_symbols_unicode <- c(
 )
 
 ipa_chars <- intToUtf8(Unicode::as.u_char(ipa_symbols_unicode), multiple = TRUE)
+
+ipa_diacritics_unicode <- c(
+  '0334', '033C', '032A', '033B', '033A', '031F', '0320', '031D', '031E',
+  '0318', '0319', '031C', '0339', '032C', '0325', '0330', '0324', '0329',
+  '032F', '0303', '0308', '033D', '0306', '031A', '02DE', '02E1', '207F',
+  '02B7', '02B2', '02E0', '02E4', '02B0', '02BC', '02D0', '02D1', '0361',
+  '02C8', '02CC', '02E5', '02E6', '02E7', '02E8', '02E9', 'A71B', 'A71C',
+  '2191', '2193', '2197', '2198', '203F', '030A', '030B', '0301', '0304',
+  '0300', '030F', '0302', '030C', '1DC4', '1DC5', '1DC6', '1DC7', '1DC8',
+  '1DC9', '035C', '0348', '0349', '0353', '032E', '0347', '02C0', '02B1',
+  '1D31'
+)
+
+diacritics_regex <- ".(\u0334|\u033C|\u032A|\u033B|\u033A|\u031F|\u0320|\u031D|\u031E|\u0318|\u0319|\u031C|\u0339|\u032C|\u0325|\u0330|\u0324|\u0329|\u032F|\u0303|\u0308|\u033D|\u0306|\u031A|\u02DE|\u02E1|\u207F|\u02B7|\u02B2|\u02E0|\u02E4|\u02B0|\u02BC|\u02D0|\u02D1|\u0361|\u02C8|\u02CC|\u02E5|\u02E6|\u02E7|\u02E8|\u02E9|\uA71B|\uA71C|\u2191|\u2193|\u2197|\u2198|\u203F|\u030A|\u030B|\u0301|\u0304|\u0300|\u030F|\u0302|\u030C|\u1DC4|\u1DC5|\u1DC6|\u1DC7|\u1DC8|\u1DC9|\u035C|\u0348|\u0349|\u0353|\u032E|\u0347|\u02C0|\u02B1|\u1D31)+"
