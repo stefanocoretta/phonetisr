9b16668fc7de2b9ef4de9e7e54d2656d8237572c
stefanocoretta <stefano.coretta@gmail.com>
2022-02-10 22:07:23 +0000

    ✨ - add featurise

--- R/featurise.R
+++ R/featurise.R
@@ -1,9 +1,25 @@
+data("ipa_symbols", envir = environment())
+
+#' Add features to list of phones
+#'
+#' This function counts occurrences of phones and includes basic phonetic features.
+#'
+#' @param phlist A list of phones or the output of `phonetise()`.
+#'
+#' @return A tibble.
+#' @export
 featurise <- function(phlist) {
     feats <- tibble::tibble(
         phone = unlist(phlist)
     ) %>%
     dplyr::count(phone, name = "count") %>%
-    dplyr::arrange(count)
+    dplyr::arrange(count) %>%
+    dplyr::mutate(
+        base = stringr::str_remove_all(
+            phone, paste0(ipa_diacritics, collapse = "|")
+        )
+    ) %>%
+    dplyr::left_join(y = ipa_symbols, by = c("base" = "IPA"))
 
     return(feats)
 }
